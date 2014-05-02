77e04dced43bce4525256628c49a36de7fcf2f40
Capi Etheriel <barraponto@gmail.com>
2014-05-01 23:15:46 -0300

    adjusting the reader class

--- reader.py
+++ reader.py
@@ -1,3 +1,4 @@
+import re
 from PIL import Image, ImageFilter
 from pyocr import tesseract
 
@@ -5,7 +6,7 @@ from pyocr import tesseract
 class SabespReader(object):
 
     @staticmethod
-    def optimizer(image):
+    def image_optimizer(image):
         return image.resize(
             [n*10 for n in image.size], Image.ANTIALIAS
         ).filter(
@@ -14,12 +15,17 @@ class SabespReader(object):
             'P', palette=Image.ADAPTIVE, colors=2
         )
 
+    @staticmethod
+    def text_optimizer(text):
+        text = text.replace(',', '.').replace('-', '.')
+        search = re.compile(r'\d+\.\d+').findall(text)
+        return search.pop() if search else 'DEBUG ' + text
+
     def __init__(self, cropper):
         self.cropper = cropper
-
-    def read(self):
-        self.regions = {name: SabespReader.optimizer(region)
-                        for name, region in self.cropper.regions.iteritems()}
+        self.regions = ((name, SabespReader.image_optimizer(region))
+                        for name, region in self.cropper.regions.iteritems())
         self.values = {name: tesseract.image_to_string(region)
-                       for name, region in self.regions.iteritems()}
-        return self.values
+                       for name, region in self.regions}
+        self.optivalues = {name: float(SabespReader.text_optimizer(text))
+                           for name, text in self.values.iteritems()}
