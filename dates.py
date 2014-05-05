823795d9cb95ed652f798f73c48b33291b404aa5
Capi Etheriel <barraponto@gmail.com>
2014-05-05 02:44:28 -0300

    generate valid combinations for dates up until today

---
+++ dates.py
@@ -0,0 +1,12 @@
+import json
+from datetime import date, timedelta
+
+start = date(2004, 1, 1)
+today = date.today()
+delta = today - start
+
+dates = [{'day': d.day, 'month': d.month, 'year': d.year}
+         for d in (start + timedelta(n) for n in xrange(delta.days))]
+
+with open('dates.json', 'w') as f:
+    json.dump(dates, f)
