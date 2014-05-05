e2d8c5bd0e4e398ea085f4fab726f6bf39d03dac
Capi Etheriel <barraponto@gmail.com>
2014-05-05 14:31:48 -0300

    check the md5sums for any timeout error or stuff like that

---
+++ checkfiles.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+import json
+from collections import defaultdict
+from hashlib import md5
+from itertools import chain
+
+
+def datename(datelike):
+    return '{year}-{month}-{day}'.format(**datelike)
+
+
+def file_md5sum(filename):
+    try:
+        with open(filename) as f:
+            return md5(f.read()).hexdigest()
+    except IOError:
+        return False
+
+
+with open('dates.json') as f:
+    dates = json.load(f)
+
+md5sums = {datename(d): file_md5sum('source/' + datename(d) + '.jpg')
+           for d in dates}
+grouped_results = defaultdict(set)
+
+for filename, result in md5sums.iteritems():
+    grouped_results[result].add(filename)
+
+rerun = list(chain.from_iterable(
+    [keys for value, keys in grouped_results.iteritems() if len(keys) > 1]))
+
+with open('rerun.json', 'w') as f:
+    json.dump(rerun, f)
